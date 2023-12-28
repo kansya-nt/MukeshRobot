257ba9f0c0650241734f701c37c8fb57dd0b2b23
esanwtel <154304688+esanwtel@users.noreply.github.com>
2023-12-28 17:01:10 +0700

    Update tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -54,7 +54,7 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f"🏓 [{usr.first_name}](tg://user?id={usr.id}), "
         if usrnum == 7:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt},\n\n{msg}"
