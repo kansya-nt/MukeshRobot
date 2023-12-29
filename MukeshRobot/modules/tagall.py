722c0c039c1244d735604fd0d23f4157958e3197
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-29 08:44:57 +0700

    A tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -55,9 +55,9 @@ async def mentionall(event):
             break
         usrnum += 1
         usrtxt += f"🏓 [{usr.first_name}](tg://user?id={usr.id})\n"
-        if usrnum == 7:
+        if usrnum == 5:
             if mode == "text_on_cmd":
-                txt = f"{usrtxt},\n\n{msg}"
+                txt = f"{msg}\n\n{usrtxt}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
