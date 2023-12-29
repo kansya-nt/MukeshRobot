b983e8bd88d90399cd48b4fa1657b0f55bf3f9e4
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-29 07:55:24 +0700

    A __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -52,7 +52,7 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Berlinmusic_support")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Asupanhot_viral")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -70,7 +70,7 @@ if ENV:
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "6185020582").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "6024180996").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
@@ -85,7 +85,7 @@ if ENV:
         raise Exception("Your tiger users list does not contain valid integers.")
 
     try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
+        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "6024180996").split())
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
